7341905a5e6d2f9ca766937136949b13b93b29d4
James Westerkamp <it@James-Westerkamps-iMac.local>
2017-05-10 20:37:04 -0500

    Started Shop

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,7 @@
+class ItemsController < ApplicationController
+
+  def shop
+    render('shop.html.erb')
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+get('/', {:controller => 'photos', :action => 'index'})
 get('/photos/:id', {:controller => 'photos', :action => 'show'})
 get('/new_photo', {:controller => 'photos', :action => 'new'})
 get('/create_photo', {:controller => 'photos', :action => 'create'})
@@ -7,4 +8,5 @@ get('/update_photo/:id', {:controller => 'photos', :action => 'update'})
 get('/photos', {:controller => 'photos', :action => 'index'})
 get('/photos/:id/destroy', {:controller => 'photos', :action => 'destroy'})
 get('/photos/:id/like', {:controller => 'photos', :action => 'like'})
+get('/shop', {:controller => 'items', :action => 'shop'})
 end
